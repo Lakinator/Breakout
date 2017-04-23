594a17591023faaa94a9c57d0fa2e33694173b93
Lukas S <SchalkLukas20@gmail.com>
2017-04-23 15:23:19 +0200

    Update Breakout.java

--- Game/Breakout.java
+++ Game/Breakout.java
@@ -2,9 +2,9 @@ package Game;
 
 import com.sun.javafx.geom.Vec2f;
 
+import java.awt.*;
 import java.awt.geom.Line2D;
 import java.util.ArrayList;
-import java.util.Random;
 import java.util.Timer;
 import java.util.TimerTask;
 
@@ -17,19 +17,19 @@ public class Breakout {
     private static boolean debug = false;
     private Gui gui;
     private Timer timer;
+    private static int score;
     public static Line2D debugLine;
     public static Line2D[] brickDebugLines;
 
     public Breakout() {
-        gui = new Gui("Breakout", 1280, 720);
+        gui = new Gui("Breakout", 1280, 720, false);
     }
 
-    public Breakout(int width, int height) {
-        gui = new Gui("Breakout", width, height);
+    public Breakout(int width, int height, boolean fullScreen) {
+        gui = new Gui("Breakout", width, height, fullScreen);
     }
 
     public void start() {
-        timer = new Timer();
 
         Ball ball = new Ball((int) gui.getDimensions().x, (int) gui.getDimensions().y);
         ball.setRandomSpeed();
@@ -56,18 +56,10 @@ public class Breakout {
         }
 
 
-
-
+        timer = new Timer();
         timer.scheduleAtFixedRate(new TimerTask() {
             @Override
             public void run() {
-
-                ball.update();
-                paddle.update();
-                ball.handleCollision((int) gui.getDimensions().x, (int) gui.getDimensions().y, bricks, gui);
-                paddle.handleCollision((int) gui.getDimensions().x, (int) gui.getDimensions().y, ball);
-
-                gui.setTitle("Breakout || Speed: " + ball.getSpeed().toString());
 
                 int counter = 0;
                 for (Brick b : bricks) {
@@ -75,15 +67,19 @@ public class Breakout {
                 }
                 if (counter == 0) stop();
 
+
+                ball.update();
+                paddle.update();
+                ball.handleCollision((int) gui.getDimensions().x, (int) gui.getDimensions().y, bricks, gui);
+                paddle.handleCollision((int) gui.getDimensions().x, (int) gui.getDimensions().y, ball);
+
                 gui.render();
+
             }
         }, 0, 15);
-
-
     }
 
     public void stop() {
-        timer.cancel();
         gui.reset();
         start();
     }
@@ -96,6 +92,14 @@ public class Breakout {
         return Breakout.debug;
     }
 
+    public static int getScore() {
+        return score;
+    }
+
+    public static void setScore(int score) {
+        Breakout.score = score;
+    }
+
     Brick[] setupBricks() {
 
         //TODO: Brickmuster machen
@@ -103,22 +107,29 @@ public class Breakout {
         int bricksizeWidth = (int) gui.getDimensions().x / 10; //Eine Reihe: 8
         int bricksizeHeight = (int) gui.getDimensions().y / 28; //Eine Spalte: 5
 
-        int howManyARow;
-
         ArrayList<Brick> bricks = new ArrayList<>();
 
         int startX = (int) (bricksizeWidth*1.1);
         int startY = (int) (bricksizeHeight*1.1);
+        int color_index = 0;
 
-        for (int i = 1; i <= 8; i++) {
-            howManyARow = new Random().nextInt(6);
-            System.out.println(howManyARow);
-
-            for (int j = 1; j <= 5; j++) {
-                if (j >= howManyARow) {
-                    bricks.add(new Brick((int) gui.getDimensions().x, (int) gui.getDimensions().y, new Vec2f(startX*j, startY*i)));
-                    System.out.println(new Vec2f(startX*j, startY*i).toString());
+        for (int i = 1; i <= 9; i++) {
+            for (int j = 1; j <= 7; j++) {
+                Color c = Color.BLACK;
+                if (color_index == 0) {
+                    c = Color.BLACK;
+                    color_index++;
                 }
+                else if (color_index == 1) {
+                    c = Color.BLUE;
+                    color_index++;
+                }
+                else if (color_index == 2) {
+                    c = Color.GREEN;
+                    color_index = 0;
+                }
+                bricks.add(new Brick((int) gui.getDimensions().x, (int) gui.getDimensions().y, new Vec2f(startX*j, startY*i), c));
+                //System.out.println(new Vec2f(startX*j, startY*i).toString());
             }
         }
 
@@ -126,23 +137,6 @@ public class Breakout {
         for (int i = 0; i < bricks.size(); i++) {
             out[i] = bricks.get(i);
         }
-
-
-
-
-
-        /*for (int i = 1; i <= 8; i++) {
-            howManyARow = new Random().nextInt(6);
-            System.out.println(howManyARow);
-
-            if (howManyARow != 0) {
-                for (int j = 1; j <= howManyARow; j++) {
-                    bricks.add(new Brick((int) gui.getDimensions().x, (int) gui.getDimensions().y, new Vec2f(startX*j, startY*i)));
-                    System.out.println(new Vec2f(startX*j, startY*i).toString());
-                }
-            }
-
-        }*/
 
 
         return out;
